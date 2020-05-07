e35c33ae42066ab11c45f4f0aed4f652ce0c06f5
Chanwoo Chung <cwchung@mit.edu>
2020-05-07 07:46:14 -0400

    badblock test update

--- badBlock.cpp
+++ badBlock.cpp
@@ -88,7 +88,7 @@ void eraseErrorCb(void *req) {
 }
 
 int main() {
-	AmfManager *am = AmfOpen(2); // Erase only mapped blocks (written blocks) so that device is clean state
+	AmfManager *am = AmfOpen(1); // Erase only mapped blocks (written blocks) so that device is clean state
 
 	SetReadCb(am, readCb, readErrorCb); // you can register NULL as a callback (ignored)
 	SetWriteCb(am, writeCb, writeErrorCb);
@@ -136,7 +136,8 @@ int main() {
 
 	clock_gettime(CLOCK_REALTIME, &start);
 	for (unsigned int i=0; i< TESTNUM; i++) {
-		AmfRead(am, i, buf, (void*)get_test_struct(i));
+		test_struct *my_req = get_test_struct(i);
+		AmfRead(am, i, my_req->buf, (void*)my_req);
 	}
 	clock_gettime(CLOCK_REALTIME, &now);
 
